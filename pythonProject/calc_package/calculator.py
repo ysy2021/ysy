c02cb4e4398726b09dd41164bc137c649270ff52
cc <645252710@qq.com>
2021-02-02 19:03:13 +0800

    第一次上传

---
+++ pythonProject/calc_package/calculator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2021/2/2 11:12
+# @Author  : ysy
+# @Software: PyCharm
+class Calculator:
+    def add(self,a,b):
+        return a+b
+    def div(self,a,b):
+        return a/b
---
+++ pythonProject/ysy_test/test_calculator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2021/2/2 11:16
+# @Author  : ysy
+# @Software: PyCharm
+import sys
+import pytest
+import yaml
+
+print(11111111111111)
+print(sys.path)
+print(2222222222)
+from calc_package.calculator import Calculator
+def get_datas():
+    with open('./datas/data.yml') as f:
+        datas=yaml.safe_load(f)
+        print(datas['add'])
+        return datas
+
+class Testcalc:
+    def setup_class(self):
+        print('测试开始咯')
+    def teardown_class(self):
+        print('测试结束')
+
+    datas1=get_datas()
+    @pytest.mark.parametrize('a,b,result',datas1['add'],ids=datas1['name'][0])
+    def test_add(self,a,b,result):
+        print(f'a= {a},b={b},c={result}')
+        calc=Calculator()
+        assert result==calc.add(a,b)
