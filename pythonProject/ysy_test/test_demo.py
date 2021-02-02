e5a89da61fd80c40a20da17bdc0f7fd16eeaac4e
cc <645252710@qq.com>
2021-02-02 19:07:08 +0800

    第一次上传

---
+++ pythonProject/ysy_test/test_demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/1/28 20:40
+# @Author  : ysy
+# @Site    : 
+# @File    : test_demo.py
+# @Software: PyCharm
+import sys
+
+print(sys.path)
+def inc(x):
+    return x + 1
+
+def test_answer():
+    assert inc(3) == 5
